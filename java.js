7348b6ad38fc4ecd93f24898368ce9dbf7b8b3f8
ikamrava <ikamrava>
2022-11-30 19:50:13 +0000

    add london and shiraz weather

--- java.js
+++ java.js
@@ -1,48 +1,110 @@
 body = document.getElementById("body")
+body.style.backgroundColor = "#E1EDEF"
 
-async function getIranTime(){
-    const res = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Tehran`)
-    const data = await res.json()
-    time = data.datetime.slice(11,-13)
-    document.getElementById("irantime").textContent = time
-}
-async function getLondonTime(){
-    const res = await fetch(`http://worldtimeapi.org/api/timezone/Europe/London`)
-    const data = await res.json()
-    time = data.datetime.slice(11,-13)
-    console.log(time)
-}
+navigator.geolocation.getCurrentPosition(position => {
+
+  const long = position.coords.longitude
+  const lat = position.coords.latitude
+  fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${long}&units=metric`)
+        
+        .then(res => res.json())
+        .then(data => {
+         
+          iconurl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+          document.getElementById("yweatherwrap").innerHTML = `
+          <p id="yname">${data.name}</p>
+          <p id="ytemp">Temp: ${Math.round(data.main.temp)}°</p>
+          <p id="yhum">Humidity: ${data.main.humidity}%</p>
+          <img id="yicon"src="${iconurl}">`
+
+        })
+
+});
+
+
+  fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=29.61&lon=52.53&units=metric`)
+        
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          iconurl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+          document.getElementById("shiraztemp").innerHTML = `
+          <p id="yname">${data.name}</p>
+          <p id="ytemp">Temp: ${Math.round(data.main.temp)}°</p>
+          <p id="yhum">Humidity: ${data.main.humidity}%</p>
+          <img id="yicon"src="${iconurl}">`
+
+        })
+
+
+
 
 
 
         fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=iran")
+        
         .then(res => res.json())
         .then(data => {
-         body.style.backgroundImage = `url(${data.urls.regular})`
-    
-
+        body.style.backgroundImage = `url(${data.urls.regular})`
     }).catch(e => {
-        body.style.backgroundImage = `url("./main.png")`
+         body.style.backgroundImage = `url("./main.png")`
+
     })
 
 
-    getIranTime()
-    getLondonTime()
+    async function getIranTime(){
+      const res = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Tehran`)
+      const data = await res.json()
+      time = data.datetime.slice(11,-16)
+      document.getElementById("irantime").textContent = time
+  }
+  async function getLondonTime(){
+      const res = await fetch(`http://worldtimeapi.org/api/timezone/Europe/London`)
+      const data = await res.json()
+      time = data.datetime.slice(11,-16)
+
+      document.getElementById("londontime").textContent = time
+
+      console.log(time)
+  }
+
+  getIranTime()
+  getLondonTime()
+
+  setInterval(getIranTime, 60000) 
+  setInterval(getLondonTime, 60000) 
+  
 
         
-  fetch("https://api.coingecko.com/api/v3/coins/01coin")   
-  .then(res => res.json())
+/*fetch("http://api.navasan.tech/latest/?api_key=freeawVldcKZKlXxKITzsMVwk3A1J9Wu ")   
+  .then(res => {
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    return res.json()
+})
   .then(data => {
-   console.log(data.market_data.current_price.gbp)
 
+    let gbp = data.gbp.value
+    let us = data.usd_sell.value
+    let seke = data.sekkeh.value
+    let date = data.gbp.date
+
+  document.getElementById("pricetoGBP").textContent = "Pond Price:   " + (gbp/1000).toFixed(3) + "   قیمت پوند"
+  document.getElementById("priceDollor").textContent = "Dollar Price:   " + (us/1000).toFixed(3)+ "   قیمت دلار"
+  document.getElementById("priceSeke").textContent = "Coin Price:   " + (seke/1000).toFixed(6)+ "   قیمت سکه"
+  document.getElementById("currency").textContent = "Currency Price on " + date
+  //  document.getElementById("icon").src = data.image.thumb
+  //  document.getElementById("coinname").textContent = data.name
+  //  console.log(data.market_data.current_price.gbp)
+  //  console.log(data.image.thumb)
+  //  console.log(data.name)
 
 }).catch(e => {
-  console.log(e)
+  document.getElementById("currency").textContent = "Currency is not available at this time"
 })
-    
-
-
-
+*/
+  
 
 
 
@@ -55,5 +117,10 @@ const invertColor = (bg) => {
   
   return bg
 }
+
+        
+
+
+
 
 
